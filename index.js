77aceb24ae01534d68ad667d494dffc4ac4f1ed0
ketul soni <108616571+ketulsoni007@users.noreply.github.com>
2023-09-23 19:06:43 +0530

    update

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Route all other requests to the client's index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
